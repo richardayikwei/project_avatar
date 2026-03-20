e204ab2804561d2883c48f5047cec953bd3b290f
richardayikwei <richardayikweibee@gmail.com>
2026-03-20 11:32:04 +0000

    further structure to code

---
+++ avatar/activities/__inti__.py
@@ -0,0 +1,3 @@
+'''
+The actions that a person can perform
+'''
---
+++ avatar/features/__init__.py
@@ -0,0 +1,4 @@
+'''
+Physical features of a person. eg height, skin color, 
+size, limbs, gender, weight, eye color, teeth, hair color 
+'''
